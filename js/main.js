d56168435db96bebfbe8de1622445c9f9393dfbe
esmagulKilincat <149780856+esmagulKilincat@users.noreply.github.com>
2023-12-10 10:26:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,134 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: false,
+        animateOut: 'fadeOutLeft',
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+
+function openModal(modalId) {
+var modal1 = document.getElementById(modalId);
+if (modal1) {
+    modal1.style.display = 'block';
+}
+}
+
+// kullanıcı giriş modalını kapama
+function closeModal(modalId) {
+var modal1 = document.getElementById(modalId);
+if (modal1) {
+    modal1.style.display = 'none';
+}
+}
+
+//Admin paneline giriş fonksiyonu
+function checkCredentials(event) {
+event.preventDefault(); // Formun otomatik olarak gönderilmesini engelle
+
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+
+// Admin bilgileri burada kontrol edilecek
+var adminMail = "admin@terafizyo.com";
+var adminPassword = "terafizyo";
+
+if (username === adminMail && password === adminPassword) {
+    window.location.href = "adminPanel.html"; // Yönlendirilecek sayfanın URL'si
+} else {
+    alert("Hatalı giriş! Lütfen doğru bilgileri girin.");
+}
+}
+
+
+    
+})(jQuery);
+
